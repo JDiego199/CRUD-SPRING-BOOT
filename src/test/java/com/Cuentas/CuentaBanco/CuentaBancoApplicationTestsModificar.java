ced3624ab0cf794e165e71b170c1a3e54aed8d79
durazno641 <87629354+durazno641@users.noreply.github.com>
2022-12-11 22:03:35 -0500

    Add files via upload

--- src/test/java/com/Cuentas/CuentaBanco/CuentaBancoApplicationTestsModificar.java
+++ src/test/java/com/Cuentas/CuentaBanco/CuentaBancoApplicationTestsModificar.java
@@ -2,29 +2,45 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.Cuentas.CuentaBanco;
+
 import com.Cuentas.CuentaBanco.model.Ahorros;
+import com.Cuentas.CuentaBanco.service.AhorrosService;
 import com.Cuentas.CuentaBanco.service.AhorrosServiceImplement;
+import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  *
  * @author fabricio
  */
 public class CuentaBancoApplicationTestsModificar {
+
     @Test
     public void TestModificacion() {
-        Ahorros ahorross = new Ahorros();
-        AhorrosServiceImplement ahorros = new AhorrosServiceImplement();
-        
-     
-        ahorros.findById(1);
-        ahorross=ahorros.findById(1);
-        ahorros.save(ahorross); 
-        assertEquals(ahorross, ahorros.findById(ahorross.getId()));
-        
+
+        AhorrosService ahorrosService = mock(AhorrosService.class);
+
+        ahorrosService.delete(1);
+        Ahorros ahorros = new Ahorros();
+        ahorros.setNumCuenta("132132");
+        ahorros.setNombre("Diego");
+        ahorros.setCedula("132326");
+        ahorros.setSaldoInicial(123);
+        ahorros.setInteres(5);
+
+        ahorrosService.delete(1);
+
+
+        ahorrosService.save(ahorros);
+
+         assertNull(ahorrosService.findById(1));
+    
 
     }
     
